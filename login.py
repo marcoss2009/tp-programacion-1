cce606fd3a8cf81110b48a5f209a4d82a9ccbcbd
Gonzalo Buglioni <gabuglioni@gmail.com>
2024-09-11 13:48:33 -0300

    login

--- login.py
+++ login.py
@@ -1,2 +1,35 @@
-def menuIngreso():
-    print("Ingreso al Sistema")
+def menuIngreso(vendedores, claves):
+    # print("Ingreso al Sistema")
+
+    vendedor = input("Ingrese su usuario: ")
+    # Comprobar que el vendedor existe
+    vendedorExiste = comprobarVendedor(vendedor, vendedores)
+    while vendedorExiste == False:
+        vendedor = input("Ingrese su usuario: ")
+        # Comprobar que el vendedor existe
+        vendedorExiste = comprobarVendedor(vendedor, vendedores)
+
+    vendedorIndice = vendedores.index(vendedor)
+
+    clave = int(input("Ingrese clave: "))
+    claveVendedor = claves(vendedorIndice)
+    chequearClave = comprobarClave(clave, claveVendedor)
+    while chequearClave == False:
+        clave = int(input("Ingrese clave: "))
+        chequearClave = comprobarClave(clave, claveVendedor)
+
+def comprobarVendedor(vendedor, vendedores):
+    existe = False
+
+    if vendedor in vendedores:
+        existe = True
+
+    return existe
+
+def comprobarClave(clave, claveVendedor):
+    resultado = False
+
+    if (clave == claveVendedor):
+        resultado = True
+
+    return resultado
